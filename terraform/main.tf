6a6066a42589b511cbfc4495a012e785bf167b58
Sacha Wharton <extern_wharton_sacha@allianz.de>
2022-10-27 12:47:17 +0200

    tf provision kind

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,75 @@
-provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "eu-central-1"
-  s3_force_path_style         = false
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
+terraform {
+  required_providers {
+      kind = {
+      source = "tehcyx/kind"
+      version = "~> 0.0.14"
+    }
 
-  endpoints {
-    apigateway     = "http://localhost:4566"
-    apigatewayv2   = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    elasticache    = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    rds            = "http://localhost:4566"
-    redshift       = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    s3             = "http://s3.localhost.localstack.cloud:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
+      kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.3.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
   }
 }
 
-resource "aws_s3_bucket" "test-bucket" {
-  bucket = "abs-bucket"
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+  name = "ortelius-in-a-box"
+  wait_for_ready = true
+  kind_config {
+    kind = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.25.2"
+    }
+  }
 }
+
+provider "kubectl" {
+  host = "${kind_cluster.default.endpoint}"
+  cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
+  client_certificate = "${kind_cluster.default.client_certificate}"
+  client_key = "${kind_cluster.default.client_key}"
+}
+
+provider "helm" {
+  kubernetes {
+    host = "${kind_cluster.default.endpoint}"
+    cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
+    client_certificate = "${kind_cluster.default.client_certificate}"
+    client_key = "${kind_cluster.default.client_key}"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name  = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "5.6.2"
+  create_namespace = true
+
+  values = [
+    file("argocd/application.yaml")
+  ]
